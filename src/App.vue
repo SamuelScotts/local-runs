5dcba8dce3473395e194279fea5a8b81946aa628
Samuel Scott <66216873+SamuelScotts@users.noreply.github.com>
2020-12-26 21:43:21 +0000

    Style amendments to Login.vue
    Style amendments made to Login.vue.
    
    Logo added.  Page structured using cols/rows grid.
    
    Background colour changed in App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app style=" background: #FAFAFA">
     <router-view></router-view>
   </v-app>
 </template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,28 @@
 <template>
-  <v-form>
-    <v-container>
-      <v-row>
-        <v-col cols="12" md="4">
-          <v-text-field v-model="loginForm.email" label="Email" required></v-text-field>
-          <v-text-field v-model="loginForm.password" label="Password" required></v-text-field>
-          <v-btn @click="login()">Login</v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col cols="12" sm="4"></v-col>
+      <v-col cols="12" sm="4">
+        <v-img class="mt-12" src="../assets/localroutes.png"></v-img>
+        <v-card class="pa-12 mt-4" outlined flat>
+          <v-form>
+            <v-container>
+              <v-row>
+                <v-col cols="12" md="12">
+                  <v-text-field v-model="loginForm.email" label="Email" required></v-text-field>
+                  <v-text-field v-model="loginForm.password" label="Password" required></v-text-field>
+                  <v-btn block class="primary mt-4" @click="login()">Login</v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="4"></v-col>
+    </v-row>
+  </v-container>
+
+
 </template>
 
 <script>
