f5586c5fbc2645343c55f4ddb1bba735c95a21d7
Samuel Scott <66216873+SamuelScotts@users.noreply.github.com>
2020-12-28 21:40:05 +0000

    Additional amendments to new OpenLayers setup

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
         <v-toolbar-title class="ml-6" style="cursor: pointer" @click="$router.push('/')"><h1>LOCAL ROUTES</h1></v-toolbar-title>
         <v-spacer></v-spacer>
         <v-select width="1" class="mt-6 mr-10" dense label="Where are you running?" solo :items="items"></v-select>
-        <v-btn  @click="$router.push('/login')" dark large>Login</v-btn>
+        <v-btn @click="$router.push('/login')" dark large>Login</v-btn>
     </v-app-bar>
   </div>
 </template>
