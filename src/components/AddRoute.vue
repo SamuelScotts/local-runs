09a8128fb12f068691c58f98ae98f4433bc2242c
Samuel Scott <66216873+SamuelScotts@users.noreply.github.com>
2020-12-29 21:40:46 +0000

    Refactored Map.vue / Minor additions to AddRoute.vue
    Refactored code in Map.vue to make it more readable/simpler.
    
    Still in the process of implementing a popup appearing when clicking on an already featured run.
    
    Added minor addition to AddRoute.vue - a file selector to upload GPX files.

--- src/components/AddRoute.vue
+++ src/components/AddRoute.vue
@@ -13,32 +13,43 @@
         </v-card-title>
 
         <v-card-text>
-            <v-form v-model="valid">
+            <v-form>
                 <v-container>
                 <v-row>
                     <v-col cols="12" md="12">
-                        <v-text-field v-model="email" label="Run name" required></v-text-field>
+                        <v-text-field prepend-icon="mdi-run" label="Run name" required></v-text-field>
                     </v-col>
                 </v-row>
                 <v-row>
                     <v-col cols="12" md="6">
-                    <v-text-field v-model="firstname" label="Latitude Coordinate" required></v-text-field>
+                    <v-text-field prepend-icon="mdi-compass" label="Latitude" required></v-text-field>
                     </v-col>
                     <v-col cols="12" md="6">
-                    <v-text-field v-model="lastname" label="Longitude Coordinate" required ></v-text-field>
+                    <v-text-field prepend-icon="mdi-compass" label="Longitude" required ></v-text-field>
                     </v-col>
                 </v-row>
                 <v-row>
                     <v-col cols="12" md="6">
-                    <v-text-field v-model="firstname" label="Distance in miles" required></v-text-field>
+                    <v-text-field prepend-icon="mdi-map-marker-distance" label="Distance in miles" required></v-text-field>
                     </v-col>
                     <v-col cols="12" md="6">
-                    <v-select width="1" class="mt-3" flat outlined dense label="Run Type" solo :items="items"></v-select>
+                      <v-file-input accept=".gpx" label="Upload GPX file" ></v-file-input>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" md="6">
+                    <v-select width="1" class="mt-3" flat outlined dense label="Run Type" solo></v-select>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                    <v-select width="1" class="mt-3" flat outlined dense label="Difficulty" solo></v-select>
                     </v-col>
                 </v-row>
                 </v-container>
             </v-form>
         </v-card-text>
+
+
+        
 
         <v-divider></v-divider>
 
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <div id="map"
-        style="width: 100%; height: 90vh;">
-    </div>
+    <div id="map" class ="mapx" style="width: 100%; height: 90vh;"></div>
     <div id="popup">
+      <v-card>
+        <v-card-text>
+          RUN TITLE
+        </v-card-text>
+        <v-card-actions>
+        <v-btn x-small>GPX</v-btn>
+        <v-btn x-small>Close</v-btn>
+        </v-card-actions>
+      </v-card>
     </div>
   </div>
 </template>
@@ -11,65 +18,87 @@
 <script>
   import View from 'ol/View'
   import Map from 'ol/Map'
-  import TileLayer from 'ol/layer/Tile'
+  import Tile from 'ol/layer/Tile'
   import OSM from 'ol/source/OSM'
   import VectorLayer from 'ol/layer/Vector'
   import VectorSource from 'ol/source/Vector'
-  import GeoJSON from 'ol/format/GeoJSON'
+  import Feature from 'ol/Feature'
+  import Point from 'ol/geom/Point'
+  import Overlay from 'ol/Overlay'
+  import {fromLonLat} from 'ol/proj';
 
   // importing the OpenLayers stylesheet is required for having
   // good looking buttons!
   import 'ol/ol.css'
 
-  const data = {
-    type: 'Feature',
-    properties: {
-      name: 'Auchterarder',
-    },
-    geometry: {
-      type: 'Point',
-      coordinates: [-3.698496, 56.292576]
-    }
-  };
-
   export default {
     name: 'Map',
-    components: {},
-    props: {},
+
+    data () {
+      return {
+        //
+      }
+    },
+
+    methods: {
+      initMap(){
+
+        let data = new Feature({
+          type: 'click',
+          name: 'Auchterarder',
+          geometry: new Point(fromLonLat([-3.698496, 56.292576]))
+        })
+        let datas = []
+        datas.push(data);
+
+        let vectorSource = new VectorSource({
+          features: datas
+        });
+  
+        let vectorLayer = new VectorLayer({
+          source: vectorSource,
+        });
+
+        const view = new View({
+            zoom: 6.5,
+            center: [-452507.207448, 7709734.866327],
+            constrainResolution: true
+          })
+
+        let map = new Map({
+          target: 'map',
+          view: view,
+          layers: [
+            new Tile({
+              source: new OSM()
+            }),
+            vectorLayer
+          ],
+          loadTilesWhileAnimating: true,
+        })
+
+        let popup = new Overlay({
+        });
+        map.addOverlay(popup)
+
+        map.on('singleclick', function(evt){
+          let feature = map.forEachFeatureAtPixel(evt.pixel, function(feature){
+            return feature;
+          })
+          
+          if(feature){
+            let coord = feature.getGeometry().getCoordinates();
+            popup.setPosition(coord)
+          }
+
+        })
+
+      },
+    
+    },
+
     mounted() {
-
-      // a feature (geospatial object) is created from the GeoJSON
-      const feature = new GeoJSON().readFeature(data, {
-        // this is required since GeoJSON uses latitude/longitude,
-        // but the map is rendered using “Web Mercator”
-        featureProjection: 'EPSG:3857'
-      });
-
-      // a new vector layer is created with the feature
-      const vectorLayer = new VectorLayer({
-        source: new VectorSource({
-          features: [feature],
-        }),
-      })
-      // this is where we create the OpenLayers map
-      new Map({
-        // the map will be created using the 'map-root' ref
-        target: 'map',
-        layers: [
-          // adding a background tiled layer
-          new TileLayer({
-            source: new OSM() // tiles are served by OpenStreetMap
-          }),
-          vectorLayer
-        ],
-
-        // the map view will initially show the whole world
-        view: new View({
-          zoom: 6.5,
-          center: [-452507.207448, 7709734.866327],
-          constrainResolution: true
-        }),
-      })
+      this.initMap()
     },
   }
 </script>
